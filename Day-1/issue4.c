0c8f7523e6548f660cadf9e8f7533d2d716cb000
Nachiketh1704 <bellamkonda.nachikethgupta@gmail.com>
2024-10-25 10:37:16 +0530

    issue1.c corrected

--- Day-1/issue4.c
+++ Day-1/issue4.c
@@ -1,15 +1,31 @@
 #include <stdio.h>
 
-void del(int arr, int element) {
-    
-    // Define a function to delelte an element from the array passed as argument.
+void del(int arr[], int size, int element) {
+    int pos = -1;
+
+    for (int i = 0; i < size; i++){
+        if (arr[i] == element){
+            pos = i;
+            break;
+        }
+    }
+
+    if (pos != -1){
+        for (int i = pos; i <size - 1; i++){
+            arr[i] = arr[i+1];
+        }
+        arr[size - 1] = 0;
+    }else{
+        printf("Element not found in the array.\n");
+    }    
 }
 
 
 int main() {
     int arr[5] = {2, 3, 4, 5, 7};
+    int size = 5;
 
-    del(arr, 5);
+    del(arr, size, 5);
 
     for (int i = 0; i < 5; i++) {
         printf("%d ", arr[i]);
